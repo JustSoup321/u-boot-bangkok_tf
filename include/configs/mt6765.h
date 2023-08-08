c7bebfd79de8dcfb4431e638eef63e6df5177bc1
Moss <82092798+JustSoup321@users.noreply.github.com>
2023-08-07 20:38:36 -0400

    Port values over from other MediaTek SOCs

--- include/configs/mt6765.h
+++ include/configs/mt6765.h
@@ -1,12 +1,15 @@
 #ifndef __MT6765_H
 #define __MT6765_H
 
-/* Uboot definition */
-#define CFG_SYS_UBOOT_BASE                   CONFIG_TEXT_BASE
+#define CFG_SYS_SDRAM_BASE		0x80000000
 
-/* SPL -> Uboot */
+#define CFG_SYS_INIT_SP_OFFSET	0x400000
+
+/* SPL */
+
 #define CFG_SYS_UBOOT_START		CONFIG_TEXT_BASE
-/* DRAM */
-#define CFG_SYS_SDRAM_BASE		0x40000000
+
+/* Dummy value */
+#define CFG_SYS_UBOOT_BASE		0
 
 #endif
